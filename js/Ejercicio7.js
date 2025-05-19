cf23fe50f6f33b218538270bf24617fd8d6e17bd
Yael-Ramos <yerg1119@gmail.com>
2025-05-19 15:50:31 -0600

    Terminado el ejercicio de registro de usuario

--- js/Ejercicio7.js
+++ js/Ejercicio7.js
@@ -1,3 +1,23 @@
 function procesarFormulario() {
 
+const form = document.getElementById("formularioCreacion");
+const resultado = document.getElementById("resultado");
+
+/*variables a obtener*/
+const nombre = form.nombre.value.trim();
+const correo = form.email.value.trim();
+
+if (!nombre || !correo) {
+    resultado.textContent = "Por favor rellenen los campos.";
+    resultado.style.color = "red";
+    resultado.style.display = "block";
+    return;
+}
+
+/*Se muestran resultados dentro de la misma pagina*/
+resultado.textContent = `Nombre: ${nombre} | Email: ${correo}`;
+resultado.style.color = "#004a99";
+resultado.style.display = 'block';
+
+
 }
